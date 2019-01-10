09efb09178f98b515f3f8a080d19a01a1e152045
Dacheng Zhao <daczhao123@gmail.com>
2019-01-10 14:50:24 -0500

    longest sub palindrome

--- 1. Hack the Algorithm Interview/200-longestSubPalindrome.py
+++ 1. Hack the Algorithm Interview/200-longestSubPalindrome.py
@@ -3,35 +3,44 @@ class Solution:
     @param s: input string
     @return: the longest palindromic substring
     """
+    # dynamics programming
     def longestPalindrome(self, s):
-        # write your code here
-        if s == "": return ""
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]  #2d array
-        res = ""
+        if not s:
+            return ''
         
-        '''
-        If the string s were 'babad', think this:
-                            j
-                        b a b a d             0 1 2 3 4
-                     b  T                  0  T
-                     a    T                1 <- T
-                i    b  T   T          i   2  T   T
-                     a    T   T            3    <-- T
-                     d          T          4          so on.
-                                            iterations go left. checks top right. Checking top right is like 
-                                            saying if u got substring 'baab' and u check top right for 'aa'.
-        '''
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        res = ''
+        
         for i in range(n):
             for j in range(i, -1, -1):
-                # if they are equal and (the substring is length 2 or 1  or  the inner substring is also palindrom by looking up-right)
-                if s[i] == s[j] and (i-j < 2 or dp[i-1][j+1]):
-                    dp[i][j] = True 
-                    
-                # if earlier if statement were True, get longer result
-                if dp[i][j] and (i + 1) - j  > len(res):
-                    res = s[j:i+1]
+                if s[i] == s[j] and (i - j < 2 or dp[i - 1][j + 1]):
+                    dp[i][j] = True
+                if dp[i][j] and (i + 1) - j > len(res):
+                    res = s[j : i + 1]
         return res
 
+    # central expand
+    def longestPalindrome2(self, s):
+        if not s:
+            return ''
+        
+        n = len(s)
+        res = ''
+
+        for middle in range(n):
+            sub = self.find_palindrome(s, middle, middle)
+            if len(sub) > len(res):
+                res = sub
+            sub = self.find_palindrome(s, middle, middle + 1)
+            if len(sub) > len(res):
+                res = sub
+        return res
+
+    def find_palindrome(self, string, left, right):
+        while left >= 0 and right < len(string) and string[left] == string[right]:
+            left -= 1
+            right += 1
+        return string[left + 1 : right]
 test = Solution()
-print(test.longestPalindrome('asdfabacabadfs'))
+print(test.longestPalindrome2('ccc'))
