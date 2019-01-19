729bee5288e3867ce13060cc56ef7ff115414831
Dacheng Zhao <daczhao123@gmail.com>
2019-01-19 11:50:02 -0500

    Cloest-bst-value done

---
+++ 4. BFS topological sort/900-cloest-bst-value.py
@@ -0,0 +1,77 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        # write your code here
+        if not root:
+            return 0
+            
+        self.node = root.val
+        self.dfs(root, target)
+        return self.node
+        
+        
+    def dfs(self, root, target):
+        if not root:
+            return 0
+        
+        if abs(target - self.node) > abs(target - root.val):
+            self.node = root.val
+        if target > root.val:
+            self.dfs(root.right, target)
+        else:
+            self.dfs(root.left, target)
+
+
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        # write your code here
+        if not root:
+            return None
+        
+        self.node = root.val 
+        
+        def dfs(root, target):
+            if not root:
+                return None
+            if abs(target - self.node) > abs(target - root.val):
+                self.node = root.val
+            if target > root.val:
+                dfs(root.right, target)
+            else:
+                dfs(root.left, target)
+                
+        dfs(root, target)
+        return self.node
+                
+        
+        
+        
+        
+        
+        
