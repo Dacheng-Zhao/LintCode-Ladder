ba8a597181ca96fe129447fcf592800118a7fbcf
Dacheng Zhao <daczhao123@gmail.com>
2019-02-11 13:31:27 -0500

    Problems solved

---
+++ 10. Dynamic Programming/111-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer
+    """
+    def climbStairs(self, n):
+        # write your code here
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        dp = [0 for _ in range(n + 1)]
+        
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
