8a5730998e245ce039ad99782a064fa3076ed016
Dacheng Zhao <daczhao123@gmail.com>
2019-03-23 22:23:42 -0400

    problem solved

---
+++ 1. Hack the Algorithm Interview/161-rotate-image.py
@@ -0,0 +1,32 @@
+class Solution:
+    """
+    @param matrix: a lists of integers
+    @return: nothing
+    """
+    def rotate(self, matrix):
+        # write your code here
+        if not matrix:
+            return []
+        
+        n = len(matrix)
+        
+        top = 0
+        left = 0
+        right = n - 1 
+        bottom = n - 1
+        
+        while n > 1:
+            for i in range(n - 1):
+                temp = matrix[top][left + i]
+                matrix[top][left + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = matrix[top + i][right]
+                matrix[top + i][right] = temp
+            top += 1
+            left += 1 
+            right -= 1 
+            bottom -= 1 
+            n -= 2
+            
+            
+        
