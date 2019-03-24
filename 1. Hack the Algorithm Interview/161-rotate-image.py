4894e0404be6048e240ffe940432f5d832c1b3e5
Dacheng Zhao <daczhao123@gmail.com>
2019-03-24 18:44:48 -0400

    second solution

--- 1. Hack the Algorithm Interview/161-rotate-image.py
+++ 1. Hack the Algorithm Interview/161-rotate-image.py
@@ -27,6 +27,13 @@ class Solution:
             right -= 1 
             bottom -= 1 
             n -= 2
+
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse() 
             
             
         
